94594f8a15b4ea9b8f53007547ba50fd2aaf73d6
teo_hong_zhang <thehotzebra@gmail.com>
2023-01-27 09:41:34 +0800

    test

---
+++ src/test/java/com/example/ssfassessment/SsfassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssfassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
